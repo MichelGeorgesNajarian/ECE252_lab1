17dba6a1835c08787815d9f54a67fea70b400cef
MichelGeorgesNajarian <migeo.najarian@gmail.com>
2019-05-30 14:55:04 -0400

    passing by reference simple_PNG - 3

--- lab1/catpng.c
+++ lab1/catpng.c
@@ -135,10 +135,12 @@ void init_iHDR(struct data_IHDR *test_iHDR, char *png_name, U32 *totalHeight, st
 
 	//doing height
 	memcpy(&test_iHDR->height, test->p_IHDR->p_data + incrementation, sizeof(test_iHDR->height));
-	incrementation += sizeof(test_iHDR->height);
 	test_iHDR->height = htonl(test_iHDR->height);
 	*(totalHeight) += test_iHDR->height;
 	test_iHDR->height = *(totalHeight); //updating max height
+	memcpy(test->p_IHDR->p_data + incrementation, &test_iHDR, sizeof(test_iHDR->height));
+	printf("------------------ Height from test: %02X%02X%02X%02X\n", *(test->p_IHDR->p_data + incrementation), *(test->p_IHDR->p_data + incrementation + 1), *(test->p_IHDR->p_data + incrementation + 2), *(test->p_IHDR->p_data + incrementation + 3));
+	incrementation += sizeof(test_iHDR->height);
 
 	//doing	bit depth
 	memcpy(&test_iHDR->bit_depth, test->p_IHDR->p_data + incrementation, sizeof(test_iHDR->bit_depth));
