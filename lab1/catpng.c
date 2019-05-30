5ac045b3b9e2f0878b2dc58d2b0966a44e49db51
MichelGeorgesNajarian <migeo.najarian@gmail.com>
2019-05-30 17:38:28 -0400

    passing by reference simple_PNG - 27

--- lab1/catpng.c
+++ lab1/catpng.c
@@ -187,11 +187,6 @@ void init_iDAT(FILE *pngFiles, char *png_name, U32 *totalHeight, struct simple_P
 	p_buffer = malloc(CHUNK_CRC_SIZE + 1); //get length of data
 	memset(p_buffer, 0, CHUNK_CRC_SIZE);
 	fread(p_buffer, 1, CHUNK_CRC_SIZE, pngFiles);
-	for (int i = 0; i < CHUNK_CRC_SIZE; i++) {
-		printf("%X", *(p_buffer + i));
-	}
-	printf("\n");
-	printf("\n");
 	free(p_buffer);
 
 	p_buffer = malloc(CHUNK_LEN_SIZE +1); //get length of data
@@ -199,12 +194,7 @@ void init_iDAT(FILE *pngFiles, char *png_name, U32 *totalHeight, struct simple_P
 
 	fread(p_buffer, 1, CHUNK_LEN_SIZE, pngFiles);
 
-	for (int i = 0; i < CHUNK_LEN_SIZE; i++) {
-		printf("%X", *(p_buffer + i));
-	}
-	printf("\n");
-
-	U32 chuck_length;
+	U32 chuck_length = *p_buffer;
 
 	printf("Chuck length %u \n", chuck_length);
 
