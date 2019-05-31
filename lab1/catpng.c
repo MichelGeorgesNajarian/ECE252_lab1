54a6eff0016fdeb4e0554efec1413d1c7a0314e1
MichelGeorgesNajarian <migeo.najarian@gmail.com>
2019-05-31 14:57:36 -0400

    passing by reference - 34

--- lab1/catpng.c
+++ lab1/catpng.c
@@ -424,10 +424,10 @@ void buildPng(struct simple_PNG *test, FILE *concatenated_png)
 
 	printf("total length of p_data: %X\n", sizeof(test->p_IDAT->p_data));
 #endif
-	fwrite(&test->p_IEND->length, 1, CHUNK_LEN_SIZE, concatenated_png);
-	fwrite(&test->p_IEND->type, 1, CHUNK_TYPE_SIZE, concatenated_png);
+	//fwrite(&test->p_IEND->length, 1, CHUNK_LEN_SIZE, concatenated_png);
+	//fwrite(&test->p_IEND->type, 1, CHUNK_TYPE_SIZE, concatenated_png);
 	//fwrite(test->p_IEND->p_data, 1, test->p_IEND->length, concatenated_png);
-	fwrite(&test->p_IEND->crc, 1, CHUNK_CRC_SIZE, concatenated_png);
+	//fwrite(&test->p_IEND->crc, 1, CHUNK_CRC_SIZE, concatenated_png);
 }
 
     /* Step 1.2: Fill the buffer with some data */
