8b6c5be5b3fc3b497388834b7a4d044f36f3b54e
MichelGeorgesNajarian <migeo.najarian@gmail.com>
2019-05-31 15:29:47 -0400

    passing by reference - 34

--- lab1/catpng.c
+++ lab1/catpng.c
@@ -157,8 +157,9 @@ void init_iHDR(struct data_IHDR *test_iHDR, char *png_name, U32 *totalHeight, st
 	p_buffer = malloc(test->p_IHDR->length);
 	memset(p_buffer, 0, test->p_IHDR->length);
 	fread(p_buffer, 1, length_ihdr, pngFiles);
-	for (int i = 0; i < test->p_IHDR->length; i++) {
+	for (int i = 0; i < length_ihdr; i++) {
 		*(test->p_IHDR->p_data + i) = *(p_buffer + i);
+		printf("%02X", *(test->p_IHDR->p_data + i));
 	}
 	free(p_buffer);
 
